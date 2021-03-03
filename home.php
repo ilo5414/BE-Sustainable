170e99eb2f518193d836d719b72f531ebf197f4c
the311thsheep <60568687+the311thsheep@users.noreply.github.com>
2021-03-03 13:50:30 +1300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,41 @@
+
+<!-- background image -->
+<!-- <div class="jumbotron home_banner" style="height:1000px;">
+
+
+</div> -->
+
+<div class="container-fluid" >
+  <div class="row">
+
+    <div class="col-4 text-white">
+      <p>hello</p>
+    </div>
+
+    <div class="col-4 text-white">
+      <p>kia ora </p>
+
+    </div>
+    <div class="col-4 text-white">
+      <p>sup</p>
+    </div>
+</div>
+<div class="row">
+
+
+
+
+
+    <form class="form-inline my-2 my-lg-0"  method="POST" action="index.php?page=searchresults" >
+      <input class="form-control mr-sm-2" type="search" name='search' placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+
+
+  </div>
+  <?php
+  // include ('barcodereader.php'); 
+  ?>
+
+
+</div>
