366c468cdec1abf7e6861a63ef6981fdb1c52700
the311thsheep <the311thsheep@users.noreply.github.com>
2021-03-17 13:33:03 +1300

    search now works for many to many

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
 
 
 
-    <form class=""  method="POST" action="index.php?page=searchresults">
+    <form class="form-inline my-2 my-lg-0"  method="POST" action="index.php?page=searchresults">
       <div class="form-group">
       <input class="form-control mr-sm-2" type="search" name='search' placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
