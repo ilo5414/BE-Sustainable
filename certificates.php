a331f0784110cf30bbdb368b027b7591891df104
ilo5414 <ilo5414@stacmail.net>
2021-03-30 09:58:41 +1300

    Certificates + aboutus page
    started putting and styling more content onto the certificates page while fixing a few problems on the way with the about page

--- certificates.php
+++ certificates.php
@@ -1,24 +1,116 @@
 
 
 
-  <div class="container-fluid" id="certificates_himg">
+  <div class="container-fluid">
+    <div class="section" id="certificates_himg">
+
+
 
     <?php include("navbar.php"); ?>
 
+    <div class="txt_align_center section">
+        <h1 class="display-4">Product Certificates!</h1>
+        <div class="section">
+          <h1 class='display-6'>What they are</h1>
+          <h1 class='display-6'>What do they mean</h1>
+          <h1 class='display-6'>Why do we need them</h1>
+        </div>
 
 
-    <div class="jumbotron">
-      <h1 class="display-4">Product Certificates!</h1>
-      <h1 class='display-6'>OUR MISSION</h1>
-      <h1 class='display-6'>WHO WE ARE</h1>
-      <hr class="my-4">
-      <p>arrow</p>
-      <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      </p>
+        <div class="section">
+          <p class="lead">
+            <a class="btn btn-lg" href="#" role="button">arrow</a>
+          </p>
+        </div>
+
     </div>
+    <!-- section header ends -->
+
+  </div>
+  <!-- section and image id ends -->
+
+  <div class="fern_img">
+    <div class="segment">
+      <div class="row section">
+
+          <div class="col-4">
+            <h1>Product certificates</h1>
+            <h1>What are they?</h1>
+          </div>
+          <!-- col-3 ends -->
+
+          <div class="col-6">
+            <p>Product certification or product qualification is the process
+              of certifying a certain product. They do this By testing it’s performance and
+              quality to assure it meets qualification criteria of the specific certification ß</p>
+          </div>
+            <!-- col-3 ends -->
+          </div>
+          <!-- row section ends -->
+        </div>
+  <!-- segment ends -->
 
 
+    <div class="segment">
+      <div class="row section">
+
+
+          <div class="col-6">
+            <p>Different certificates mean different things aka different certificates have
+              different criteria that products have to meet in order to be certified.
+              Read up below on the certificates that our products have to learn more about them
+              and what they mean for the product you have purchased. </p>
+          </div>
+            <!-- col-3 ends -->
+          </div>
+          <!-- row section ends -->
+        </div>
+  <!-- segment ends -->
+
+
+        <div class="row sb_cards">
+          <div class="col-3" >
+            <div class="card">
+              <h1>Fair trade</h1>
+              <p>One of the three componets of sustainbility here we want to make
+                companies accountable for their actions but also endorse the companies
+                that go the extra mile</p>
+            </div>
+          </div>
+          <!-- end of col-3 -->
+
+          <div class="col-3">
+            <div class="card">
+              <h1>SPCA</h1>
+              <p>Our environmental footprint is huge and we want to encourage people to buy
+                products that helo emilminate that and force companies to make </p>
+              </div>
+          </div>
+          <!--end of col-3 -->
+
+          <div class="col-3">
+            <div class="card">
+              <h1>USDA organic</h1>
+              <p>Why buy a product that only benefits yourself when you can purchase a product that
+              not benefits you but also people in need. </p>
+            </div>
+          </div>
+          <!-- end of col-3 -->
+
+          <div class="col-3">
+            <div class="card">
+              <h1>green tick</h1>
+              <p>Why buy a product that only benefits yourself when you can purchase a product that
+              not benefits you but also people in need. </p>
+            </div>
+          </div>
+          <!-- end of col-3 -->
+
+        </div>
+        <!-- end of row sb_cards -->
+
+      </div>
+      <!-- fern_img  ends -->
 
 
   </div>
