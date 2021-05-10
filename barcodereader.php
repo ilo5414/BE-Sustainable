b14aef7b1ab8aa8fbf2cd47a1475e591b7c2948a
the311thsheep <the311thsheep@users.noreply.github.com>
2021-05-11 08:52:40 +1200

    failed barcode reader

--- barcodereader.php
+++ barcodereader.php
@@ -1,57 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>CodePen - SnapShop - EAN reader based on QUAGGA</title>
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
-<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'><link rel="stylesheet" href="./style.css">
-
-</head>
-<body>
-<!-- partial:index.partial.html -->
-<div class="container" style="margin-top: 2em;">
-
-<div class="row">
-	<div class="col-lg-12">
-		<div class="input-group">
-			<input id="scanner_input" class="form-control" placeholder="Click the button to scan an EAN..." type="text" />
-			<span class="input-group-btn">
-				<button class="btn btn-default" type="button" data-toggle="modal" data-target="#livestream_scanner">
-					<i class="fa fa-barcode"></i>
-				</button>
-			</span>
-		</div><!-- /input-group -->
-	</div><!-- /.col-lg-6 -->
-</div><!-- /.row -->
-<div class="modal" id="livestream_scanner">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-				<h4 class="modal-title">Barcode Scanner</h4>
-			</div>
-			<div class="modal-body" style="position: static">
-				<div id="interactive" class="viewport"></div>
-				<div class="error"></div>
-			</div>
-			<div class="modal-footer">
-				<label class="btn btn-default pull-left">
-					<i class="fa fa-camera"></i> Use camera app
-					<input type="file" accept="image/*;capture=camera" capture="camera" class="hidden" />
-				</label>
-				<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-			</div>
-		</div><!-- /.modal-content -->
-	</div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-</div>
-<!-- partial -->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js'></script>
-<script src='https://a.kabachnik.info/assets/js/libs/quagga.min.js'></script><script  src="./script.js"></script>
-
-</body>
-</html>
+<iframe src="https://codesandbox.io/embed/barcode-scanner-0ksx3?fontsize=14&hidenavigation=1&theme=dark"
+     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+     title="Barcode-Scanner"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+   ></iframe>
