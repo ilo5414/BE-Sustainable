bb63a072d859724bbee0df7fca3bbcd154fef15e
the311thsheep <the311thsheep@users.noreply.github.com>
2021-05-20 17:05:34 +1200

    send to window

--- barcodereader.php
+++ barcodereader.php
@@ -2,14 +2,41 @@
 include("navbar.php");
 ?>
 <div class="row">
-<iframe src="https://zxing-ngx-scanner.stackblitz.io/" title="barcode scanner" allow="geolocation; microphone; camera" width="100%" height="500" frameborder="0"></iframe>
+<!-- <iframe id="printf" name="printf" src="https://zxing-ngx-scanner-mbkcv7.stackblitz.io" title="barcode scanner" allow="geolocation; microphone; camera" width="100%" height="500" frameborder="0"></iframe> -->
 </div>
-<button onclick="myFunction()">Click me</button>
+<!-- <button type="button" onclick="myFunction()">Try it</button>
+<h1 id="out"></h1>
 <script type="text/javascript">
 function myFunction() {
+  var myframe = document.getElementById("myFrame");
+  var bcode = document.getElementID("barcode");
   document.write("hi");
-  var iframe = document.getElementByTagName("strong");
-  exec(document.getElementByTagName("strong");)
+  document.write(bcode); -->
+
+<!--
 }
+</script> -->
+<iframe id="frame" src="https://zxing-ngx-scanner-mbkcv7.stackblitz.io" style="border: none; height: 350px; width: 100%"></iframe>
+
+   <div style="padding: 1rem">
+       <h2>Window</h2>
+       <div id="message" style="border: 1px solid #cbd5e0; height: 12rem; margin: .5rem 0; overflow: auto; padding: .5rem"></div>
+   </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+   const messageEle = document.getElementById('message');
+   const frameEle = document.getElementById('frame');
+
+   window.addEventListener('message', function(e) {
+       const data = JSON.parse(e.data);
+       const date = new Date(data.date).toLocaleTimeString('en-US');
+
+       messageEle.innerHTML = `Receive "${data.message}" at ${date}<br>` + messageEle.innerHTML;
+   });
+
+   
+});
 </script>
+
 <?php include("display.php"); ?>
