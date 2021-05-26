4ba28f5633074f9c14252106fd0cac725b5c15a1
ilo5414 <ilo5414@stacmail.net>
2021-05-26 13:56:15 +1200

    updated admin page

--- admin.php
+++ admin.php
@@ -8,8 +8,41 @@ if(!isset($_SESSION['admin'])) {
 
 else {
 ?>
-<h1>Welcome User xxxx</h1>
-<p><a href = "index.php?page=logout">logout</a></p>
+
+<div class="container-fluid" id="homepage_himg">
+
+  <?php include("navbar.php"); ?>
+
+
+<div class="jumbotron"  >
+
+
+<div class="row">
+
+  <div class="col-12">
+
+  <h1>Welcome User xxxx</h1>
+  <p><a href = "index.php?page=logout">logout</a></p>
+
+  </div>
+
+</div>
+<!-- end of row 1 -->
+
+<div class="row">
+  <div class="col-6">
+    <h1>Favourite products</h1>
+
+  </div>
+
+  <div class="col-6">
+    <h1>Favourite certificates</h1>
+    
+  </div>
+</div>
+
+</div>
+<!-- end of container-fluid -->
 
 
 <?php
