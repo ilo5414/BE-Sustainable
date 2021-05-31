4fddb79feebe600af56e5d126b94ff441a46ce48
the311thsheep <the311thsheep@users.noreply.github.com>
2021-06-01 09:35:19 +1200

    more star stuff

--- starinsertitem.php
+++ starinsertitem.php
@@ -3,9 +3,14 @@
 
 $certID = $_POST['certID'];
 $userID = $_POST['userID'];
+$checked = $_POST['starcheck'];
+
+
 echo "$userID";
 echo "$certID";
 
+
+if ($checked='starunchecked') {
 
 
     // inserts into database
@@ -19,5 +24,16 @@ echo "$certID";
       } else {
         echo "Error: " . $sql . "<br>" . $dbconnect->error;
       }
-  
+}else {
+  // inserts into database
+    $sql = "DELETE FROM `favcert` WHERE `favcert`.`userID` = $userID AND `favcert`.`certID` = $certID";
+
+    if ($dbconnect->query($sql) == TRUE) {
+  //if insert succesful, go to homepage
+      header('Location: index.php?page=certificates');
+
+    } else {
+      echo "Error: " . $sql . "<br>" . $dbconnect->error;
+    }
+}
   ?>
