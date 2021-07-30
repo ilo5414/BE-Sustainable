7c3cce9cd8d7badac4f1339e46450f0fa41640b9
the311thsheep <the311thsheep@users.noreply.github.com>
2021-07-30 15:42:06 +1200

    h

--- livesearch.php
+++ livesearch.php
@@ -15,7 +15,7 @@ if (isset($_GET['productID'])) {
 
 <?php
    // the sql stament that will be run in the data base to obtain the information wanted
-   $product_sql = "SELECT * FROM products $displaycondition";
+   $product_sql = "SELECT * FROM products JOIN company ON products.companyID=company.companyID $displaycondition";
  // this takes the slq written above to the data base and runs it to obtain the information wanted
    $product_qry = mysqli_query($dbconnect, $product_sql);
  // this turns the inforamtion retrieved into an assosiative array
@@ -27,6 +27,7 @@ if (mysqli_num_rows($product_qry)>0) {
  do {
    $n=$n+1;
    $product_name = $product_aa['productname'];
+   $company_name = $product_aa['companyname'];
    $product_barcode = $product_aa['productbarcode'];
    $productID = $product_aa['productID'];
 
@@ -37,8 +38,8 @@ if (mysqli_num_rows($product_qry)>0) {
 <div class="">
 
 
-     <p><?php echo $product_name ?></h1>
-
+     <p><?php echo $product_name ?></p>
+    <p><?php echo $company_name ?></p>
 <p><?php echo $product_barcode ?></p>
 </div>
 </div>
--- searchresults.php
+++ searchresults.php
@@ -1,3 +1,22 @@
+<script type="text/javascript">
+function showResult(str) {
+  if (str.length==0) {
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
+    return;
+  }
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("livesearch").innerHTML=this.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  var displaycondition = "WHERE productname LIKE '%"+str+"%' OR productbarcode LIKE '%"+str+"%'"
+  xmlhttp.open("GET","livesearch.php?displaycondition="+displaycondition+"&prodcolno=2",true);
+  xmlhttp.send();
+}
+</script>
 
 <?php
 include("navbar.php");
@@ -9,18 +28,18 @@ include("navbar.php");
 
   $search = $_POST['search'];
   ?>
+<div class="row d-flex justify-content-center" style="margin-bottom:0px;">
 
-  <div class="row d-flex justify-content-center">
 
-
+  <!-- page displays food tiems -->
   <!-- searh items bar -->
   <form class="form-inline my-2 my-lg-0"  method="POST" action="index.php?page=searchresults">
     <div class="form-group">
-    <input class="form-control mr-sm-2" type="search" name='search' placeholder="Search" aria-label="Search">
+    <input class="form-control mr-sm-2" type="search" name='search' placeholder="Search" aria-label="Search" onkeyup="showResult(this.value)">
+
     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
   </div>
   </form>
-
   <!-- dropdown -->
   <div class="dropdown">
     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -41,63 +60,38 @@ include("navbar.php");
     </div>
   </div>
   </div>
-
+  <div class="justify-content-center" style="margin-left:auto; margin-right: auto; width:48%;" id="livesearch"></div>
 
   <h1> search results for "<?php echo "$search"; ?> " </h1> <?php
+
+
 // selects search query from database
   $result_sql = "SELECT * FROM products WHERE products.productname LIKE '%$search%' OR products.productbarcode LIKE '%$search%';";
   $result_qry = mysqli_query($dbconnect, $result_sql);
-
-  if(mysqli_num_rows($result_qry)==0) {
-    // no results error message
-      echo "<h1>No results found</h1>";
-    } else {
-      $result_aa = mysqli_fetch_assoc($result_qry);
-
-// displays result name, photo
 ?>
-<!-- all results are in a row -->
-<div class="row">
-<?php
-      do {
-        $productID = $result_aa["productID"];
-        $name = $result_aa['productname'];
-        $barcode = $result_aa['productbarcode'];
-        ?>
-
-<!-- student card -->
-
-        <div class="card col-3" style="color:black;">
-          <a href="index.php?page=productpage&productID=<?php echo "$productID"; ?>">
-          <!-- img -->
-          <img class="card-img-top" src="product_images/<?php echo $name; ?>.png" alt="<?php echo $name; ?>.png">
-          <div class="card-body">
-            <!-- name -->
-            <h5 class="card-title"><?php echo "$name $barcode"; ?></h5>
-            <?php
-
-            $cert_sql = "SELECT * FROM productcert JOIN products ON products.productID=productcert.productID JOIN cert ON cert.certID=productcert.certID WHERE products.productID LIKE '$productID';";
-            $cert_qry = mysqli_query($dbconnect, $cert_sql);
-            if(mysqli_num_rows($cert_qry)==0) {
-              // no results error message
-                echo "<p>No certificates found</p>";
-              } else {
-            $cert_aa = mysqli_fetch_assoc($cert_qry);
-            do {
-            $cert = $cert_aa['certname'];?>
-            <p class="card-title"><?php echo "$cert";?></p>
-          <?php } while($cert_aa = mysqli_fetch_assoc($cert_qry));
-        }
-
-      ?>
-          </div>
-          </a>
-        </div>
-
-      <?php
-        } while ($result_aa = mysqli_fetch_assoc($result_qry));
-?></div><?php
-
+  <!-- display food items -->
+  <div class="display">
+    <?php
+  // type name is in html request
+    if (isset($_GET['type_name'])) {
+      $type_name = $_GET['type_name'];
+    } else {
+      $type_name = 'all';
+    }
+  echo "displaying $type_name";
+  if ($type_name=="all"){
+    $displaycondition = "";
+  }else{
+    $displaycondition = "JOIN type ON type.typeID = products.typeID WHERE typename = '$type_name'";
   }
+$prodcolno = 3;
+$sendingpage = "searchresults&type_name=$type_name";
+$displaycondition="WHERE products.productname LIKE '%$search%' OR products.productbarcode LIKE '%$search%';";
+     include("display.php");
+?>
 
- ?>
+
+</div>
+
+</div>
+<!-- end of container-fluid -->
