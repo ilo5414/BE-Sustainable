5170c068d9ac2f537d58527732cf5cc44ae052d1
ilo5414 <60874571+ilo5414@users.noreply.github.com>
2021-10-11 14:32:39 +1300

    commented log out page

--- logout.php
+++ logout.php
@@ -1,11 +1,14 @@
-
+<!-- This is the logout page -->
 
   <?php
+// this ends the session
   // unset($_SESSION['admin']);
   session_destroy();
   ?>
+<!-- displays that the user has been logged out -->
   <div class="alert alert-primary" role="alert">
   You have logged out
 </div>
 <?php
+// this sends the user back to the previous page they were on
 header('Location: ' . $_SERVER['HTTP_REFERER']); ?>
